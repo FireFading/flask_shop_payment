3ae4ff53509968131fcfd24673d7a243a61dbbf6
FireFading <svetl.rudnewa2014@yandex.ru>
2023-04-14 18:19:12 +0300

    update

---
+++ README.md
@@ -0,0 +1,6 @@
+## example flask app with payment
+## to create models:
+- `export FLASK_APP=main`
+- ```from main import Product, db
+    db.create_all()
+```
--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from cloudipsp import Api, Checkout
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///./shop.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,10 +14,10 @@ class Product(db.Model):
     cost = db.Column(db.Integer, nullable=False)
     available = db.Column(db.Boolean, default=True)
     description = db.Column(db.Text, nullable=True)
-    
+
     def __repr__(self):
         return self.name
-    
+
 
 @app.route("/")
 def index():
@@ -35,9 +36,9 @@ def create():
         name = request.form["name"]
         cost = request.form["cost"]
         description = request.form["description"]
-        
+
         product = Product(name=name, cost=cost, description=description)
-        
+
         try:
             db.session.add(product)
             db.session.commit()
@@ -46,20 +47,19 @@ def create():
             return "error"
     else:
         return render_template("create.html")
-    
+
 
 @app.route("/buy/<int:id>")
 def buy(id):
     product = Product.query.get(id)
-    api = Api(merchant_id=1396424, secret_key='test')
+    api = Api(merchant_id=1396424, secret_key="test")
     checkout = Checkout(api=api)
     data = {
         "currency": "USD",
         "amount": (str(product.cost) + "00"),
     }
-    url = checkout.url(data).get('checkout_url')
+    url = checkout.url(data).get("checkout_url")
     return redirect(url)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
